0ebde3f646974e9f7a807e74a7e30ba747b8d838
Mir Al Masud <almasudme@gmail.com>
2021-11-08 01:33:02 +0000

    modified:   kth.cpp

---
+++ leetcode/kth.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+cout<<"find kth element of an array";
+int k = 3;
+
+int array[6] = {3,6,34,8,2,9};
+
+cout <<k<<"th element is "<<array[k-1];
+
+return 0;
+
+}
